92c0ce74b2a8aed9e2faec401fa91fdc9279774e
Mahmoud Ayman <mahmoud.ayman07@gmail.com>
2026-04-09 17:42:53 +0200

    refactor: implemented LedManager module

---
+++ lib/LedManager/LedManager.cpp
@@ -0,0 +1,127 @@
+#include "LedManager.h"
+
+LedManager* LedManager::instance = nullptr;
+
+LedManager::LedManager() {
+    instance = this;
+}
+
+void LedManager::begin() {
+    Serial.println("LedManager: Initializing FastLED...");
+    FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
+    FastLED.setBrightness(150);
+    FastLED.clear(true);
+    FastLED.show();
+    updateTicker();
+}
+
+void LedManager::setEffect(EffectType effect) {
+    Serial.printf("LedManager: Mode Change: %d\n", (int)effect);
+    currentEffect = effect;
+    updateTicker();
+}
+
+EffectType LedManager::getEffect() { 
+    return currentEffect; 
+}
+
+void LedManager::setSpeed(float speed) {
+    animationSpeed = speed;
+    updateTicker();
+}
+
+float LedManager::getSpeed() { 
+    return animationSpeed; 
+}
+
+void LedManager::setBrightness(uint8_t b) {
+    FastLED.setBrightness(b);
+    FastLED.show();
+}
+
+void LedManager::togglePower() {
+    bool isOn = FastLED.getBrightness() > 0;
+    setBrightness(isOn ? 0 : 150);
+    Serial.printf("LedManager: Power: %s\n", isOn ? "OFF" : "ON");
+}
+
+void LedManager::updateTicker() {
+    animationTicker.detach();
+    float intervalMs = ((1.0f - animationSpeed) + 0.1f) * 25.0f;
+    animationTicker.attach_ms(intervalMs, LedManager::updateCallback);
+}
+
+void LedManager::updateCallback() {
+    if (instance) instance->runCurrentMode();
+}
+
+void LedManager::runMusicSync(int rawAudioLevel) {
+    if (currentEffect != EFFECT_MUSIC) return;
+    int ledLevel = constrain(map(rawAudioLevel, 0, 3000, 0, NUM_LEDS), 0, NUM_LEDS);
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    for (int i = 0; i < ledLevel; ++i) leds[i] = CHSV(gHue + i * 10, 255, 255);
+    ++gHue; 
+    FastLED.show();
+}
+
+void LedManager::runCurrentMode() {
+    switch (currentEffect) {
+        case EFFECT_STATIC:      solidPulse(); break;
+        case EFFECT_RAINBOW:     rainbowDance(); break;
+        case EFFECT_BREATHING:   breathingEffect(); break;
+        case EFFECT_SPARKLE:     sparkleEffect(); break;
+        case EFFECT_COLOR_CYCLE: colorCycleEffect(); break;
+        case EFFECT_STROBING:    strobeEffect(); break;
+        case EFFECT_STARRY_NIGHT: starryNightEffect(); break;
+        case EFFECT_MUSIC:       break; 
+        default: break;
+    }
+}
+
+// Private Animation Methods
+void LedManager::solidPulse() { 
+    fill_solid(leds, NUM_LEDS, baseColor); 
+    FastLED.show(); 
+}
+
+void LedManager::rainbowDance() { 
+    fill_rainbow(leds, NUM_LEDS, gHue, 10); 
+    gHue += 5; 
+    FastLED.show(); 
+}
+
+void LedManager::breathingEffect() {
+    breatheBrightness += breatheDelta;
+    if (breatheBrightness <= 0 || breatheBrightness >= 255) breatheDelta = -breatheDelta;
+    CRGB cur = baseColor; 
+    cur.nscale8_video(breatheBrightness);
+    fill_solid(leds, NUM_LEDS, cur); 
+    FastLED.show();
+}
+
+void LedManager::sparkleEffect() { 
+    if (random8() < 60) leds[random16(NUM_LEDS)] = CRGB::White; 
+    fadeToBlackBy(leds, NUM_LEDS, 20); 
+    FastLED.show(); 
+}
+
+void LedManager::colorCycleEffect() { 
+    static uint8_t hue = 0; 
+    fill_solid(leds, NUM_LEDS, CHSV(hue++, 255, 255)); 
+    FastLED.show(); 
+}
+
+void LedManager::strobeEffect() {
+    static bool on = false; 
+    static uint32_t last = 0;
+    uint16_t dur = (1.0f - animationSpeed) * 900 + 50;
+    if (millis() - last >= dur) { on = !on; last = millis(); }
+    fill_solid(leds, NUM_LEDS, on ? CRGB::Yellow : CRGB::Black); 
+    FastLED.show();
+}
+
+void LedManager::starryNightEffect() { 
+    if (random8() < 60) leds[random(NUM_LEDS)] = CHSV(random8(), 200, 255); 
+    fadeToBlackBy(leds, NUM_LEDS, 20); 
+    FastLED.show(); 
+}
---
+++ lib/LedManager/LedManager.h
@@ -0,0 +1,49 @@
+#ifndef LED_MANAGER_H
+#define LED_MANAGER_H
+
+#include <Arduino.h>
+#include <FastLED.h>
+#include <Ticker.h>
+#include "Config.h"
+
+class LedManager {
+private:
+    CRGB leds[NUM_LEDS];
+    Ticker animationTicker;
+    
+    EffectType currentEffect = EFFECT_RAINBOW;
+    float animationSpeed = 0.5f;
+    uint8_t gHue = 0;
+    CRGB baseColor = CRGB::Blue;
+    
+    int breatheBrightness = 0;
+    int breatheDelta = 2;
+    
+    static LedManager* instance;
+
+    // Animation Methods
+    void solidPulse();
+    void rainbowDance();
+    void breathingEffect();
+    void sparkleEffect();
+    void colorCycleEffect();
+    void strobeEffect();
+    void starryNightEffect();
+
+    static void updateCallback();
+
+public:
+    LedManager();
+    void begin();
+    void setEffect(EffectType effect);
+    EffectType getEffect();
+    void setSpeed(float speed);
+    float getSpeed();
+    void setBrightness(uint8_t b);
+    void togglePower();
+    void updateTicker();
+    void runMusicSync(int rawAudioLevel);
+    void runCurrentMode();
+};
+
+#endif
