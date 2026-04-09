f4250eced1add70992a8ea968628b6a80b8374a2
Mahmoud Ayman <mahmoud.ayman07@gmail.com>
2026-04-09 17:02:26 +0200

    created system manager module

---
+++ include/SystemManager.h
@@ -0,0 +1,48 @@
+
+#ifndef SYSTEM_MANAGER_H
+#define SYSTEM_MANAGER_H
+
+#include <Arduino.h>
+#include <Preferences.h>
+#include "Config.h"
+
+class SystemManager {
+private:
+    Preferences prefs;
+    uint8_t activeMode;
+
+public:
+    SystemManager() : activeMode(MODE_SD) {}
+
+    void begin() {
+        Serial.println("[SystemManager] Initializing NVS...");
+        prefs.begin("player", false);
+        activeMode = prefs.getUChar("mode", MODE_SD);
+        Serial.printf("[SystemManager] Last stored mode: %s\n", (activeMode == MODE_BT ? "Bluetooth" : "SD Card"));
+    }
+
+    uint8_t getActiveMode() {
+        return activeMode;
+    }
+
+    void setActiveMode(uint8_t mode) {
+        activeMode = mode;
+    }
+
+    void storeModeAndReboot(uint8_t newMode) {
+        Serial.printf("[SystemManager] Saving new mode (%d) and rebooting...\n", newMode);
+        prefs.putUChar("mode", newMode);
+        prefs.end();
+        delay(100);
+        ESP.restart();
+    }
+
+    void logDiagnostics() {
+        Serial.println("--- King-H System Diagnostics ---");
+        Serial.printf("Reset Reason: %d\n", (int)esp_reset_reason());
+        Serial.printf("Heap Free: %u bytes\n", ESP.getFreeHeap());
+        Serial.println("---------------------------------");
+    }
+};
+
+#endif
